cc6aea39a4f1764076ae9c06f9403ed43c8fc569
nicolas-riera <nicolas.riera@laplateforme.io>
2026-05-26 12:30:06 +0200

    feat: added db connector and marker placement

--- resources/script/script.js
+++ resources/script/script.js
@@ -1,3 +1,8 @@
+const SUPABASE_URL = 'https://xkgdwqfldzqzsahyvicf.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_T2lUSzY9d5N1PWMt0cveEg_uJIdoJoO'; 
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
 const maxBounds = L.latLngBounds(
     L.latLng(-90, -180), 
     L.latLng(90, 180)    
@@ -15,6 +20,27 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-const example_marker = L.marker([43.2965,  5.3698]).addTo(map);
+async function loadUsersMarkers() {
+    const { data: users, error } = await supabaseClient
+        .from('User') 
+        .select('Name, Latitude, Longitude, Description');
 
-example_marker.bindPopup("<b>Nom du contributeur</b><br>Description du contributeur.");
+    if (error) {
+        console.error('Error loading users :', error);
+        return;
+    }
+
+    users.forEach(user => {
+        if (user.Latitude && user.Longitude) {
+            const lat = parseFloat(user.Latitude);
+            const lng = parseFloat(user.Longitude);
+
+            const marker = L.marker([lat, lng]).addTo(map);
+
+            const popupContent = `<b>${user.Name}</b><br>${user.Description || "Pas de description."}`;
+            marker.bindPopup(popupContent);
+        }
+    });
+}
+
+loadUsersMarkers();
